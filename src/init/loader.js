ce954fae23d3ca54a629fa72920821319516e135
Tomasz Zwierzyński <tomasz0zwierzynski@gmail.com>
2019-08-11 23:42:28 +0200

    uzupełniono dane gb i magazynu

--- src/init/loader.js
+++ src/init/loader.js
@@ -88,17 +88,53 @@ function createBuildingData(db) {
 
     buildings.insert({
         id: Buildings.MAIN_BUILDING, name: 'Main Building', singleton: true, levels: [
-            { attr: { reduction: 0 }, wood: 60, clay: 80, iron: 50, crop: 30, pop: 0, cp: 0, time: 3, requirements: [] },
-            { attr: { reduction: 50 }, wood: 300, clay: 300, iron: 300, crop: 200, pop: 3, cp: 2000, time: 6, requirements: [] }, 
-            { attr: { reduction: 75 }, wood: 900, clay: 900, iron: 900, crop: 700, pop: 7, cp: 5000, time: 12, requirements: [] },
+            { attr: { reduction: 0 }, wood: 70, clay: 40, iron: 60, crop: 20, pop: 0, cp: 0, time: 2000 / 1000, requirements: [] },
+            { attr: { reduction: 0 }, wood: 90, clay: 50, iron: 75, crop: 25, pop: 2, cp: 2000, time: 2620 / 1000, requirements: [] }, 
+            { attr: { reduction: 4 }, wood: 115, clay: 65, iron: 100, crop: 35, pop: 3, cp: 3000, time: 3340 / 1000, requirements: [] },
+            { attr: { reduction: 7 }, wood: 145, clay: 85, iron: 125, crop: 40, pop: 4, cp: 3000, time: 4170 / 1000, requirements: [] },
+            { attr: { reduction: 10 }, wood: 190, clay: 105, iron: 160, crop: 55, pop: 5, cp: 4000, time: 5140 / 1000, requirements: [] },
+            { attr: { reduction: 14 }, wood: 240, clay: 135, iron: 205, crop: 70, pop: 6, cp: 5000, time: 7570 / 1000, requirements: [] },
+            { attr: { reduction: 17 }, wood: 310, clay: 175, iron: 265, crop: 90, pop: 8, cp: 6000, time: 9080 / 1000, requirements: [] },
+            { attr: { reduction: 20 }, wood: 395, clay: 225, iron: 340, crop: 115, pop: 10, cp: 7000, time: 10830 / 1000, requirements: [] },
+            { attr: { reduction: 23 }, wood: 505, clay: 290, iron: 430, crop: 145, pop: 12, cp: 9000, time: 12140 / 1000, requirements: [] },
+            { attr: { reduction: 25 }, wood: 645, clay: 370, iron: 555, crop: 185, pop: 14, cp: 10000, time: 15220 / 1000, requirements: [] },
+            { attr: { reduction: 28 }, wood: 825, clay: 470, iron: 710, crop: 235, pop: 16, cp: 12000, time: 17950 / 1000, requirements: [] }, // Level 10
+            { attr: { reduction: 31 }, wood: 1060, clay: 605, iron: 905, crop: 300, pop: 18, cp: 15000, time: 21130 / 1000, requirements: [] },
+            { attr: { reduction: 33 }, wood: 1355, clay: 775, iron: 1160, crop: 385, pop: 20, cp: 18000, time: 24810 / 1000, requirements: [] },
+            { attr: { reduction: 36 }, wood: 1735, clay: 990, iron: 1485, crop: 495, pop: 22, cp: 21000, time: 29080 / 1000, requirements: [] },
+            { attr: { reduction: 38 }, wood: 2220, clay: 1270, iron: 1900, crop: 635, pop: 24, cp: 26000, time: 34030 / 1000, requirements: [] },
+            { attr: { reduction: 40 }, wood: 2840, clay: 1625, iron: 2435, crop: 810, pop: 26, cp: 31000, time: 41230 / 1000, requirements: [] },
+            { attr: { reduction: 42 }, wood: 3635, clay: 2075, iron: 3115, crop: 1040, pop: 29, cp: 37000, time: 46440 / 1000, requirements: [] },
+            { attr: { reduction: 44 }, wood: 4650, clay: 2660, iron: 3990, crop: 1330, pop: 32, cp: 44000, time: 54170 / 1000, requirements: [] },
+            { attr: { reduction: 46 }, wood: 5955, clay: 3405, iron: 5105, crop: 1700, pop: 35, cp: 53000, time: 63130 / 1000, requirements: [] },
+            { attr: { reduction: 48 }, wood: 7620, clay: 4355, iron: 6535, crop: 2180, pop: 38, cp: 64000, time: 79500 / 1000, requirements: [] },
+            { attr: { reduction: 50 }, wood: 9999999999, clay: 9999999999, iron: 9999999999, crop: 9999999999, pop: 41, cp: 77000, time: 9999999999, requirements: [] } // Level 20
         ]
     });
 
     buildings.insert({
         id: Buildings.WAREHOUSE, name: 'Warehouse', singleton: false, levels: [
-            { attr: { capacity: 0 }, wood: 60, clay: 80, iron: 50, crop: 30, pop: 0, cp: 0, time: 3, requirements: [] },
-            { attr: { capacity: 12000 }, wood: 300, clay: 300, iron: 300, crop: 200, pop: 3, cp: 2000, time: 600, requirements: [] }, 
-            { attr: { capacity: 17000 }, wood: 900, clay: 900, iron: 900, crop: 700, pop: 7, cp: 5000, time: 1200, requirements: [] },
+            { attr: { capacity: 0 }, wood: 130, clay: 160, iron: 90, crop: 40, pop: 0, cp: 0, time: 2000, requirements: [] },
+            { attr: { capacity: 12000 }, wood: 165, clay: 205, iron: 115, crop: 50, pop: 1, cp: 1000, time: 2620 / 1000, requirements: [] }, 
+            { attr: { capacity: 17000 }, wood: 215, clay: 260, iron: 145, crop: 65, pop: 2, cp: 1000, time: 3340, requirements: [] },
+            { attr: { capacity: 23000 }, wood: 275, clay: 335, iron: 190, crop: 85, pop: 3, cp: 2000, time: 4170, requirements: [] }, 
+            { attr: { capacity: 31000 }, wood: 350, clay: 430, iron: 240, crop: 105, pop: 4, cp: 2000, time: 5140, requirements: [] },
+            { attr: { capacity: 40000 }, wood: 445, clay: 550, iron: 310, crop: 135, pop: 5, cp: 2000, time: 6260, requirements: [] }, 
+            { attr: { capacity: 50000 }, wood: 570, clay: 705, iron: 395, crop: 175, pop: 6, cp: 3000, time: 7570, requirements: [] },
+            { attr: { capacity: 63000 }, wood: 730, clay: 900, iron: 505, crop: 225, pop: 7, cp: 4000, time: 9080, requirements: [] }, 
+            { attr: { capacity: 78000 }, wood: 935, clay: 1155, iron: 650, crop: 290, pop: 8, cp: 4000, time: 10830, requirements: [] },
+            { attr: { capacity: 96000 }, wood: 1200, clay: 1475, iron: 830, crop: 370, pop: 9, cp: 5000, time: 12860, requirements: [] }, 
+            { attr: { capacity: 118000 }, wood: 1535, clay: 1890, iron: 1065, crop: 470, pop: 10, cp: 6000, time: 15220, requirements: [] }, // Level 10
+            { attr: { capacity: 144000 }, wood: 1965, clay: 2420, iron: 1360, crop: 605, pop: 12, cp: 7000, time: 17950, requirements: [] }, 
+            { attr: { capacity: 176000 }, wood: 2515, clay: 3095, iron: 1740, crop: 775, pop: 14, cp: 9000, time: 21130, requirements: [] },
+            { attr: { capacity: 214000 }, wood: 3220, clay: 3960, iron: 2230, crop: 990, pop: 16, cp: 11000, time: 24810, requirements: [] }, 
+            { attr: { capacity: 259000 }, wood: 4120, clay: 5070, iron: 2850, crop: 1270, pop: 18, cp: 13000, time: 29080, requirements: [] },
+            { attr: { capacity: 313000 }, wood: 5275, clay: 6490, iron: 3650, crop: 1625, pop: 20, cp: 15000, time: 34030, requirements: [] }, 
+            { attr: { capacity: 379000 }, wood: 6750, clay: 8310, iron: 4675, crop: 2075, pop: 22, cp: 18000, time: 39770, requirements: [] },
+            { attr: { capacity: 457000 }, wood: 8640, clay: 10635, iron: 5980, crop: 2660, pop: 24, cp: 22000, time: 46440, requirements: [] }, 
+            { attr: { capacity: 551000 }, wood: 11060, clay: 13610, iron: 7655, crop: 3405, pop: 26, cp: 27000, time: 54170, requirements: [] },
+            { attr: { capacity: 664000 }, wood: 14155, clay: 17420, iron: 9800, crop: 4355, pop: 28, cp: 32000, time: 63130, requirements: [] }, 
+            { attr: { capacity: 800000 }, wood: 9999999999, clay: 9999999999, iron: 9999999999, crop: 9999999999, pop: 30, cp: 38000, time: 9999999999, requirements: [] }, // Level 20
         ]
     });
 
